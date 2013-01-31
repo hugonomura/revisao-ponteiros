991e92e48457d4fb1724bd7d5f33b191359dd505
Hugo Nomura <hugonomura@gmail.com>
2013-01-31 01:13:09 -0200

    Apenas explicando melhor o exercicio 105.

--- ex105.c
+++ ex105.c
@@ -4,14 +4,14 @@ int main(){
 	int i, j, *p_1, *p_2, **p_p_1, **p_p_2;
 	i = 4;
 	j = 5;
-	printf("p_1: %p\n", p_1);
-	printf("&i: %p\n", &i);
-	p_1 = &i;
-	p_2 = &j;
-	p_p_1 = &p_2;
-	p_p_2 = &p_1;
+	printf("p_1: %p\n", p_1); // p_1 contém o valor de um endereço arbitrário de memória (lixo)
+	printf("&i: %p\n", &i); // i tem um espaço de memória reservado para ela
+	p_1 = &i; // p_1 agora contém o endereço de memória de i
+	p_2 = &j; // p_2 agora contém o endereço de memória de j
+	p_p_1 = &p_2; // p_p_1 contém o endereço de memória de p_2, que, por sua vez, contém o endereço de memória de j
+	p_p_2 = &p_1; // p_p_2 contém o endereço de memória de p_1, que, por sua vez, contém o endereço de memória de i
 
-	printf("p_1 = &i: %p\n", p_1);
+	printf("p_1 = &i: %p\n", p_1); // apenas para mostrar que deu certo
 
 	return 0;
 }
