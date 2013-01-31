5d8fbd013cb39e68614bc8a5fa8c153ba8b62557
Hugo Nomura <hugonomura@gmail.com>
2013-01-31 10:40:03 -0200

    Mostrando que o endereço de um ponteiro não é alterado.

--- ex105.c
+++ ex105.c
@@ -5,6 +5,7 @@ int main(){
 	i = 4;
 	j = 5;
 	printf("p_1: %p\n", p_1); // p_1 contém o valor de um endereço arbitrário de memória (lixo)
+	printf("&p_1: %p\n", &p_1); // endereco inical de p_1
 	printf("&i: %p\n", &i); // i tem um espaço de memória reservado para ela
 	p_1 = &i; // p_1 agora contém o endereço de memória de i
 	p_2 = &j; // p_2 agora contém o endereço de memória de j
@@ -12,7 +13,7 @@ int main(){
 	p_p_2 = &p_1; // p_p_2 contém o endereço de memória de p_1, que, por sua vez, contém o endereço de memória de i
 
 	printf("p_1 = &i: %p\n", p_1); // apenas para mostrar que deu certo
-
+	printf("&p_1: %p\n", &p_1); // mostrando que o endereço de p_1 não muda
 	return 0;
 }
 
